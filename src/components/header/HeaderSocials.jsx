82fe9389bb77ac329d93575655390cbeeec8fcda
Maryjoy Tan <94347525+tanmaryjoy0824@users.noreply.github.com>
2022-10-25 09:25:51 +0800

    fixed social links

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,9 +4,9 @@ import {BsFacebook, BsInstagram, BsBehance} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="www.facebook.com/tanmmaryjoy/" target="_blank"><BsFacebook/></a>
-        <a href="www.instagram.com/umayunchan/" target="_blank"><BsInstagram/></a>
-        <a href="www.behance.net/tanmaryjoy" target="_blank"><BsBehance/></a>
+        <a href="https://www.facebook.com/tanmmaryjoy/" target="_blank" rel="noreferrer"><BsFacebook/></a>
+        <a href="https://www.instagram.com/umayunchan/" target="_blank" rel="noreferrer"><BsInstagram/></a>
+        <a href="https://www.behance.net/tanmaryjoy" target="_blank" rel="noreferrer"><BsBehance/></a>
     </div>
   )
 }
